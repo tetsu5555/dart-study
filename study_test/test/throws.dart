a58d038b4988701123cb1a3ac2e49e6b41874d37
tetsu5555 <tetete1118@gmail.com>
2020-06-26 01:14:26 +0900

    add tests

--- study_test/test/throws.dart
+++ study_test/test/throws.dart
@@ -2,6 +2,7 @@ import 'package:test/test.dart';
 
 main() {
   test('.parse() fails on invalid input', () {
+    // ここ() =>で渡すの何で何だっけ？
     expect(() => int.parse('X'), throwsFormatException);
   });
 }
