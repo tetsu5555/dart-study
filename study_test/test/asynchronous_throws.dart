14646cd9cf7422e46b2bbb00b603409e24bd6905
tetsu5555 <tetete1118@gmail.com>
2020-06-26 12:57:26 +0900

    fix

--- study_test/test/asynchronous_throws.dart
+++ study_test/test/asynchronous_throws.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:test/test.dart';
 
+// throwsA() matcherとthrowsExceptionType matcherはsynchronous callbackでもasynchronous Futureでも動く
 void main() {
   test('Future.error() throws the error', () {
     expect(Future.error('oh no'), throwsA(equals('oh no')));
