f8dd200cc2670a305b304b2d7421c22ad4f54195
tetsu5555 <tetete1118@gmail.com>
2020-06-27 19:53:36 +0900

    delete comment

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -4,7 +4,6 @@ import './screens/slack_message_list.dart';
 
 void main() => runApp(MyApp());
 
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
   static const String _title = 'これはSlackです';
 
